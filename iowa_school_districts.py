109295e8b115b4dcbe64a69a0d9a4df0d55cdaf3
nrobertson1992 <nrobertson@edx.org>
2021-12-23 10:52:44 -0500

    Add files via upload

--- iowa_school_districts.py
+++ iowa_school_districts.py
@@ -13,8 +13,8 @@ iowa_district_graduations = pd.read_csv('iowa_district_graduations.csv')
 
 
 # Update columns.
-iowa_district_expenditures.columns = ['fiscal_year','actual_re_estimated_budget','aea_dist','dist','de_dist','district_name','column_name', 'fund','source','expenditures_per_pupil','amount','enrollment_category','enrollment_category_number']
-iowa_district_graduations.columns = ['graduating_class','fall_freshman_year','district','district_name','graduates','total_cohorts','graduation_rate','graduation_rate_category']
+iowa_district_expenditures.columns = ['fiscal_year','actual_re_estimated_budget','aea','dist','de_dist','district_name','column_name', 'fund','source','expenditures_per_pupil','amount','enrollment_category','enrollment_category_number']
+iowa_district_graduations.columns = ['graduating_class','fall_freshman_year','district','district_name','graduates','total_cohort','graduation_rate','graduation_rate_category']
 
 st.title('SQL for Practical Statistics: Iowa School Districts')
 
@@ -33,19 +33,19 @@ with st.sidebar.expander('Table Reference: iowa_district_expenditures', expanded
 			Data is provided by school districts to the Department of Education to complete their Certified 
 			Annual Report (CAR).
 
-			* FiscalYear: Fiscal Year of Expenditure.
-			* Actual_ReEstimated_Budget: Actual - Data is from the Certified Annual Report ReEstimated - Data is for the budget year when the file is submitted to the DOM on April 15. Budget - Budget year or upcoming year when the file is submitted to the DOM on April 15.
-			* AEA: Area Education Agency Number.
-			* Dist: Department of Management District Number.
-			* DE_District: Department of Education District Number.
-			* District Name: School District Name.
-			* Column Name: Original Column Name of Data.
-			* Fund: School District Fund.
-			* Source: Source of Expenditure.
-			* Expenditures Per Pupil: Expenditures divided by the budget enrollment for the fiscal year.
-			* Amount: Amount of Expenditure.
-			* Enrollment Category: Enrollment Category based on district budget enrollment for the fiscal year.
-			* Enrollment Category Number: Enrollment category number. One equals lowest enrollment, Six is the highest enrollment.
+			* fiscal_year: Fiscal Year of Expenditure.
+			* actual_re_estimated_budget: Actual - Data is from the Certified Annual Report ReEstimated - Data is for the budget year when the file is submitted to the DOM on April 15. Budget - Budget year or upcoming year when the file is submitted to the DOM on April 15.
+			* aea: Area Education Agency Number.
+			* dist: Department of Management District Number.
+			* de_dist: Department of Education District Number.
+			* district_name: School District Name.
+			* column_name: Original Column Name of Data.
+			* fund: School District Fund.
+			* source: Source of Expenditure.
+			* expenditures_per_pupil: Expenditures divided by the budget enrollment for the fiscal year.
+			* amount: Amount of Expenditure.
+			* enrollment_category: Enrollment Category based on district budget enrollment for the fiscal year.
+			* enrollment_category_number: Enrollment category number. One equals lowest enrollment, Six is the highest enrollment.
 
 			[More info here.](https://data.iowa.gov/School-Finance/Iowa-School-District-Expenditures-by-Fiscal-Year/uutu-bzs3)
 			""")
@@ -57,13 +57,14 @@ with st.sidebar.expander('Table Reference: iowa_district_graduations', expanded=
 			with a group of students entering ninth grade for the first time. The cohort is adjusted to add students that 
 			transfer in and subtract students that transfer out during a four year time period for calculating a graduation rate.
 
-			* Graduating Class: Year the class is expected to graduate.
-			* Fall Freshman Year: The year the cohort entered their freshman year of high school.
-			* District: ID given by the Department of Education for the school district.
-			* District Name: Name of the school district. Name of a school district associated with a district number may change over time due to school consolidations.
-			* Graduates: First time freshman in year indicated in fall freshman year and transfers into the cohort in grades 9 to 12 who graduated in year indicated in graduating class or earlier. SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
-			* Total Cohort: First time freshman in year indicated in fall freshman year and transfers into the cohort in grades 9 to 12 less those who transferred out (including deceased). SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
-			* Graduation Rate: Graduates divided by Total Cohort multiplied by 100. SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
+			* graduating_class: Year the class is expected to graduate.
+			* fall_freshman_year: The year the cohort entered their freshman year of high school.
+			* district: ID given by the Department of Education for the school district.
+			* district_name: Name of the school district. Name of a school district associated with a district number may change over time due to school consolidations.
+			* graduates: First time freshman in year indicated in fall freshman year and transfers into the cohort in grades 9 to 12 who graduated in year indicated in graduating class or earlier. SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
+			* total_cohort: First time freshman in year indicated in fall freshman year and transfers into the cohort in grades 9 to 12 less those who transferred out (including deceased). SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
+			* graduation_rate: Graduates divided by Total Cohort multiplied by 100. SCS (small cell size) indicates data was redacted to ensure privacy standards where met.
+			* graduation_rate_category: Range of graduation rates created to form categories for summary purposes.
 
 			[More info here.](https://data.iowa.gov/Primary-Secondary-Ed/4-Year-Graduation-Rates-in-Iowa-by-Cohort-and-Publ/tqti-3w6t)
 			""")
